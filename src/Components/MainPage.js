46c089b407c7bdaa92a8e3c3eaadf1ddca1562df
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2023-05-31 12:46:40 +0530

    toy shop using react

---
+++ src/Components/MainPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Body from "./Body";
+
+const MainPage = (props) => {
+  return (
+    <>
+      <div className="main">
+        <div id="nav">
+          <div id="optionDiv">
+            <Link to="/home">Home</Link>
+            <Link to="/about">About Us</Link>
+            <Link to="/contact">Contact Us</Link>
+          </div>
+          <div id="iconDiv">
+            <img src="iconToy.png" alt="" />
+            <span>Hello, User</span>|<span>Accounts and Details</span>
+          </div>
+        </div>
+        <div id="searchDiv">
+          <div id="search">
+            <input id="searchInp" />
+            <button onClick={props.click}>Search</button>
+          </div>
+        </div>
+        <marquee
+          behavior="scroll"
+          direction="left"
+          style={{ color: "dodgerblue" }}
+        >
+          <i class="fa fa-thumbs-up"></i>
+          <i>
+            We worry about what a child will become tomorrow, yet we forget that
+            he is someone today...
+          </i>
+          <i class="fa fa-thumbs-up"></i>
+        </marquee>
+      </div>
+      <Body match={props.match} div={props.div} />
+    </>
+  );
+};
+
+export default MainPage;
---
+++ src/Components/SweetPage.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SweetPage = (props) => {
+  return (
+    <>
+      <div id="nav">
+        <div id="optionDiv">
+          <Link to="/home">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
+        </div>
+        <div id="iconDiv">
+          <img src="iconToy.png" alt="" />
+          <span>Hello, User</span>|<span>Accounts and Details</span>
+        </div>
+      </div>
+      <div className="main">
+        <div id="nav" style={{ justifyContent: "flex-end" }}>
+          <div id="cartIcon">
+            <i
+              class="fa fa-heart"
+              style={{ fontSize: "24px", color: "dodgerblue" }}
+            ></i>
+            <i
+              onClick={props.Cart}
+              class="fa fa-shopping-cart"
+              style={{ fontSize: "24px", color: "dodgerblue" }}
+            ></i>
+          </div>
+        </div>
+        {props.match.map((item) => {
+          return (
+            <div id="sweetImg" key={item.name}>
+              <img src={item.image} alt="" />
+              <div id="sweetContent">
+                <span>{item.name}</span>
+                <span>{item.price}</span>
+                <p>{item.describe}</p>
+                <button id="cart" onClick={props.add} val={item.id}>
+                  Add to cart
+                </button>
+                <div id="back"></div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default SweetPage;
